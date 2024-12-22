0a8e50d2075ced503edb4a0a3ca09579e590c7aa
Galdin Ivan <galdin.2005@mail.ru>
2024-12-22 17:24:04 +0300

    update3

--- CharpTask6/CharpTask6/obj/Debug/net9.0/CharpTask6.AssemblyInfo.cs
+++ CharpTask6/CharpTask6/obj/Debug/net9.0/CharpTask6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CharpTask6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38f5d47d46f95efec67c9053a3d88df6f7cc2b79")]
 [assembly: System.Reflection.AssemblyProductAttribute("CharpTask6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CharpTask6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CharpTask6/Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using static Task_6.Program;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void TestFermatValidCase()
+        {
+            
+            ClassicAssert.IsFalse(CheckFermat(3, 4, 5, 3));
+        }
+
+        [Test]
+        public void TestFermatInvalidCase()
+        {
+           
+            ClassicAssert.IsFalse(CheckFermat(1, 2, 3, 2));
+        }
+
+        [Test]
+        public void TestFermatEdgeCase()
+        {
+            
+            ClassicAssert.IsTrue(CheckFermat(1, 2, 3, 1));
+        }
+
+        [Test]
+        public void TestFermatWithZero()
+        {
+            
+            ClassicAssert.IsFalse(CheckFermat(0, 0, 0, 2));
+        }
+    }
+}
